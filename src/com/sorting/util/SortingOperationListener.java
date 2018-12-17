d478cb9f12bdbc752788e897abd1cd40b9fcf8df
Adam <adamsg93@hotmail.com>
2018-12-17 18:23:59 +0100

    Threading Update
    Continued work on the threading issues. Still needs work.

--- src/com/sorting/util/SortingOperationListener.java
+++ src/com/sorting/util/SortingOperationListener.java
@@ -3,6 +3,8 @@ package com.sorting.util;
 import com.sorting.program.SortingOperation;
 
 public interface SortingOperationListener {
-	void operationEvent(SortingOperation op);
-	void operationRequestRedraw(SortingOperation op);
+	void operationEvent(SortingOperation op, String message);
+	void operationRequestRedraw(SortingOperation op, int i);
+	void operationRequestRedraw(SortingOperation op, int i, int j);
+	void operationRequestRedrawAll(SortingOperation op);
 }
